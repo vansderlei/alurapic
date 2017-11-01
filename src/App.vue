319d423d083010315d5ebcec750705bd04f1df0f
vansderlei <vanderleigomes.silva@hotmail.com>
2017-10-31 23:10:00 -0200

    subindo arquivos

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
-<template>
-    <div class="corpo">
-        <h1 class="centralizado">{{titulo}}</h1>
-        <ul class="lista-fotos">
-            <li class="lista-fotos-itens" v-for="foto of fotos">
-                <img :src="foto.url" :alt="foto.titulo">
-            </li>
-        </ul>
-    </div>
-</template>
-<script>
+<!-- alurapic/src/App.vue -->
 
+<template>
+  <div class="corpo">
+
+    <h1 class="titulo">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto in fotos">
+
+        <div class="painel">
+          <h2 class="painel-titulo">{{foto.titulo}}</h2>
+          <div class="painel-corpo">
+
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+
+          </div><!-- fim painel-corpo -->
+        </div><!-- fim painel -->
+
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
 
 export default {
     data(){
@@ -26,20 +40,50 @@ export default {
  }
 }
 </script>
-
 <style>
-.corpo{
-    font-family: verdana;
-    width: 96%;
-    margin: 0 auto;
-}
-.centralizado{
+
+  .titulo {
     text-align: center;
-}
-.lista-fotos{
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
     list-style: none;
-}
-.lista-fotos .lista-fotos-itens{
+  }
+
+  .lista-fotos .lista-fotos-item {
     display: inline-block;
-}
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
+  /* estilo do painel */
+
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
 </style>
